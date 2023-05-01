711a1ee10051d39f7830778ea4424c387d232561
Donghwan Shin <d.shin@sheffield.ac.uk>
2023-05-01 11:52:43 +0100

    Solution for Task 1 (Extract methods)

--- src/main/java/example/project/Refactoring/ExtractMethods.java
+++ src/main/java/example/project/Refactoring/ExtractMethods.java
@@ -2,52 +2,32 @@ package example.project.Refactoring;
 
 public class ExtractMethods {
 
-    /**
-     * This is a method to reengineer using `extract methods` refactoring technique.
-     * TODO: complete `foo_after_refactoring()` method below using the refactoring.
-     */
-    public static void foo_before_refactoring() {
-        // initialise an array of random numbers with the size of 10
-        int[] array1 = new int[10];
-        for (int i = 0; i < array1.length; i++) {
-            array1[i] = (int) (Math.random() * 10);
+    static int[] initialiseArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10);
         }
+        return array;
+    }
 
-        // print the array with a given delimiter
-        String delimiter = " ";
-        for (int i = 0; i < array1.length; i++) {
-            System.out.print(array1[i] + delimiter);
-        }
-        System.out.println();
-
-        // initialise another array of random numbers with the size of 10
-        int[] array2 = new int[10];
-        for (int i = 0; i < array2.length; i++) {
-            array2[i] = (int) (Math.random() * 10);
-        }
-
-        // print the array with a given delimiter
-        for (int i = 0; i < array2.length; i++) {
-            System.out.print(array2[i] + delimiter);
+    static void printArrayWithDelimiter(int[] array, String delimiter) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + delimiter);
         }
         System.out.println();
     }
 
     public static void foo_after_refactoring() {
-        // TODO: uncomment the following lines and complete missing methods
+        // initialise an array of random numbers with the size of 10
+        int[] array1 = initialiseArray(10);
 
-//        // initialise an array of random numbers with the size of 10
-//        int[] array1 = initialiseArray(10);
-//
-//        // print the array with a given delimiter
-//        printArrayWithDelimiter(array1, " ");
-//
-//        // initialise another array of random numbers with the size of 10
-//        int[] array2 = initialiseArray(10);
-//
-//        // print the array with a given delimiter
-//        printArrayWithDelimiter(array2, " ");
+        // print the array with a given delimiter
+        printArrayWithDelimiter(array1, " ");
+
+        // initialise another array of random numbers with the size of 10
+        int[] array2 = initialiseArray(10);
+
+        // print the array with a given delimiter
+        printArrayWithDelimiter(array2, " ");
     }
-
-
 }
